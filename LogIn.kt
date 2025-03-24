67aed8967a1716f11b87bb24707c6703325566b1
navi955 <nr1851159@gmail.com>
2025-03-24 13:16:58 +0530

    Add files via upload

---
+++ LogIn.kt
@@ -0,0 +1,70 @@
+package com.example.chatapplication
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Email
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+
+class LogIn : AppCompatActivity() {
+
+    private lateinit var edtEmail: EditText
+    private lateinit var edtPassword: EditText
+    private lateinit var btnLogin: Button
+    private lateinit var btnSignUp: Button
+    private lateinit var mAuth: FirebaseAuth
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_log_in)
+
+        supportActionBar?.hide()
+
+        mAuth = FirebaseAuth.getInstance()
+
+        edtEmail = findViewById(R.id.edt_email)
+        edtPassword = findViewById(R.id.edt_password)
+        btnLogin = findViewById(R.id.btnLogin)
+        btnSignUp = findViewById(R.id.btnSignUp)
+
+        btnSignUp.setOnClickListener{
+            val intent = Intent(this,SignUp::class.java)
+            startActivity(intent)
+        }
+
+        btnLogin.setOnClickListener {
+            val email = edtEmail.text.toString()
+            val password = edtPassword.text.toString()
+
+            login(email,password);
+        }
+    }
+
+    private fun login(email: String, password: String){
+        // login for logging user
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // code for logging in user
+
+                    val intent = Intent(this@LogIn,MainActivity::class.java)
+                    finish()
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this@LogIn,"User does not exist",Toast.LENGTH_SHORT).show()
+
+                }
+            }
+
+
+    }
+}
